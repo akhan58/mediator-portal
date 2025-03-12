ec361f8813023f691ebf786487b8668287fcf635
Thien Hoang <thienxhoang2021@gmail.com>
2025-03-12 17:36:08 -0500

    Update disputes.sql

--- dbSchema/disputes.sql
+++ dbSchema/disputes.sql
@@ -1,10 +1,3 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 17.4
--- Dumped by pg_dump version 17.4
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -21,11 +14,6 @@ SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
---
--- TOC entry 220 (class 1259 OID 16397)
--- Name: disputes; Type: TABLE; Schema: public; Owner: postgres
---
-
 CREATE TABLE public.disputes (
     "dispute_ID" integer NOT NULL,
     "review_ID" integer NOT NULL,
@@ -33,23 +21,10 @@ CREATE TABLE public.disputes (
     dispute_status smallint DEFAULT 0 NOT NULL
 );
 
-
 ALTER TABLE public.disputes OWNER TO postgres;
-
---
--- TOC entry 4810 (class 0 OID 0)
--- Dependencies: 220
--- Name: COLUMN disputes.dispute_status; Type: COMMENT; Schema: public; Owner: postgres
---
 
 COMMENT ON COLUMN public.disputes.dispute_status IS '0 = not resolved
 1 = resolved';
-
-
---
--- TOC entry 219 (class 1259 OID 16396)
--- Name: Disputes_dispute_ID_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 ALTER TABLE public.disputes ALTER COLUMN "dispute_ID" ADD GENERATED ALWAYS AS IDENTITY (
     SEQUENCE NAME public."Disputes_dispute_ID_seq"
@@ -60,55 +35,17 @@ ALTER TABLE public.disputes ALTER COLUMN "dispute_ID" ADD GENERATED ALWAYS AS ID
     CACHE 1
 );
 
-
---
--- TOC entry 4804 (class 0 OID 16397)
--- Dependencies: 220
--- Data for Name: disputes; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
 COPY public.disputes ("dispute_ID", "review_ID", flagged_reason, dispute_status) FROM stdin;
 \.
 
-
---
--- TOC entry 4811 (class 0 OID 0)
--- Dependencies: 219
--- Name: Disputes_dispute_ID_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
 SELECT pg_catalog.setval('public."Disputes_dispute_ID_seq"', 1, false);
-
-
---
--- TOC entry 4654 (class 2606 OID 16404)
--- Name: disputes Disputes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.disputes
     ADD CONSTRAINT "Disputes_pkey" PRIMARY KEY ("dispute_ID");
 
-
---
--- TOC entry 4655 (class 1259 OID 16428)
--- Name: idx_disputes_reviewID; Type: INDEX; Schema: public; Owner: postgres
---
-
 CREATE INDEX "idx_disputes_reviewID" ON public.disputes USING btree ("review_ID") WITH (deduplicate_items='true');
 
-
---
--- TOC entry 4656 (class 1259 OID 16429)
--- Name: idx_disputes_status; Type: INDEX; Schema: public; Owner: postgres
---
-
 CREATE INDEX idx_disputes_status ON public.disputes USING btree (dispute_status) WITH (deduplicate_items='true');
-
-
---
--- TOC entry 4657 (class 2606 OID 16405)
--- Name: disputes review_ID_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.disputes
     ADD CONSTRAINT "review_ID_fkey" FOREIGN KEY ("review_ID") REFERENCES public.reviews("review_ID") ON UPDATE CASCADE ON DELETE CASCADE;
