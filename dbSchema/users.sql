6d2c3975a7c254c828deb4eb2dd844c6e676e943
Thien Hoang <thienxhoang2021@gmail.com>
2025-03-12 17:44:54 -0500

    Update users.sql

--- dbSchema/users.sql
+++ dbSchema/users.sql
@@ -1,10 +1,3 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 17.4
--- Dumped by pg_dump version 17.4
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -21,11 +14,6 @@ SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
---
--- TOC entry 224 (class 1259 OID 16433)
--- Name: users; Type: TABLE; Schema: public; Owner: postgres
---
-
 CREATE TABLE public.users (
     "user_ID" integer NOT NULL,
     username character varying(255) NOT NULL,
@@ -36,11 +24,6 @@ CREATE TABLE public.users (
 
 ALTER TABLE public.users OWNER TO postgres;
 
---
--- TOC entry 223 (class 1259 OID 16432)
--- Name: Users_user_ID_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
 ALTER TABLE public.users ALTER COLUMN "user_ID" ADD GENERATED ALWAYS AS IDENTITY (
     SEQUENCE NAME public."Users_user_ID_seq"
     START WITH 1
@@ -50,30 +33,10 @@ ALTER TABLE public.users ALTER COLUMN "user_ID" ADD GENERATED ALWAYS AS IDENTITY
     CACHE 1
 );
 
-
---
--- TOC entry 4800 (class 0 OID 16433)
--- Dependencies: 224
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
 COPY public.users ("user_ID", username, email, password) FROM stdin;
 \.
 
-
---
--- TOC entry 4806 (class 0 OID 0)
--- Dependencies: 223
--- Name: Users_user_ID_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
 SELECT pg_catalog.setval('public."Users_user_ID_seq"', 1, false);
-
-
---
--- TOC entry 4653 (class 2606 OID 16439)
--- Name: users Users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
---
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT "Users_pkey" PRIMARY KEY ("user_ID");
