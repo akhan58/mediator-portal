607d75bff4e8903fc4a7da705242e8ffd311398f
ThienXHoang <thienxhoang2021@gmail.com>
2025-04-30 13:00:58 -0500

    Updated users.js

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,30 @@
-const users = []; // Temporary in-memory store (replace with DB in Phase 4)
-// This is a temporary in-memory array to store user data. In Phase 4,
-// you’ll replace this with a proper database like MongoDB or PostgreSQL.
+const pool = require('../config/db');
+
+//TODO -- adjust authController to use these functions
+const users = {
+    async getUsers ({username}) {
+        const results = await pool.query(`SELECT * FROM users where "username" = $1`,
+            [username]
+        );
+
+        return results.rows[0];
+    },
+
+    async createUsers ({username, email, password}) {
+        const results = await pool.query(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING user_id, username, email`,
+            [username, email, password]
+        );
+
+        return results.rows[0];
+    },
+
+    async deleteUser(userId) {
+        const results = await pool.query(`DELETE FROM users WHERE "user_id" = $1`,
+            [userId]
+        );
+        
+        return results.rowCount > 0;
+    }
+}
 
 module.exports = users;
-//This exports the users array so it can be used in other files (e.g., in your routes or controllers).
